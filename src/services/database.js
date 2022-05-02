15130c6a222f36826911846f594cbbdbed744d14
Janet Mbugua <mbuguaj@live.unc.edu>
2022-05-01 20:26:04 -0400

    tired

--- src/services/database.js
+++ src/services/database.js
@@ -1,41 +1,38 @@
 const database = require('better-sqlite3')
 
 const fs = require('fs');
-const data = './data/';
+const datadir = './data/';
 
-if (!fs.existsSync(data)){
-    fs.mkdirSync(data);
+if (!fs.existsSync(datadir)){
+    fs.mkdirSync(datadir);
 }
 
-const logdb = new database(data+'log.db')
+const logdb = new database(datadir+'log.db')
 
 const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 let row = stmt.get();
 if (row === undefined) {
-// Echo information about what you are doing to the console.
-    console.log('Your database appears to be empty. I will initialize it now.');
-// Set a const that will contain your SQL commands to initialize the database.
+    console.log('Log database appears to be empty. Creating log database...')
+
     const sqlInit = `
-    CREATE TABLE accesslog ( 
-        id INTEGER PRIMARY KEY, 
-        remoteaddr TEXT, 
-        remoteuser TEXT, 
-        time TEXT, 
-        method TEXT, 
-        url TEXT, 
-        protocol TEXT, 
-        httpversion TEXT, 
-        status TEXT, 
-        referer TEXT, 
-        useragent TEXT );
-    `;
-// Execute SQL commands that we just wrote above.
-    db.exec(sqlInit);
-// Echo information about what we just did to the console.
-    console.log('Your database has been initialized with a new table and two entries containing a username and password.');
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT,
+            remoteuser TEXT,
+            time TEXT,
+            method TEXT,
+            url TEXT,
+            protocol TEXT,
+            httpversion TEXT,
+            status TEXT, 
+            referrer TEXT,
+            useragent TEXT
+        );
+    `
+
+    logdb.exec(sqlInit)
 } else {
-// Since the database already exists, echo that to the console.
-    console.log('Database exists.')
+    console.log('Log database exists.')
 }
-// Export all of the above as a module so that we can use it elsewhere.
-module.exports = db
+
+module.exports = logdb
