6f03b8bf9eec2eef8eaf91dfbd46e362a84c17ca
Janet Mbugua <mbuguaj@live.unc.edu>
2022-05-01 20:20:25 -0400

    correction

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
-// Place your server entry point code here
-// Server port
-var express = require("express");
-var app = express();
-
-const fs = require('fs');
-const morgan = require('morgan');
-const db = require("./database.js");
-const args = require('minimist')(process.argv.slice(2));
-
-console.log(args);
-app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-
-var HTTP_PORT = args.port || process.env.port || 5555;
+const args = require('minimist')(process.argv.slice(2))
 
 const help = (`
+server.js [options]
 --port, -p	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535. Defaults to 5000.
-
+            between 1 and 65535.
 --debug, -d If set to true, creates endlpoints /app/log/access/ which returns
             a JSON access log from the database and /app/error which throws 
             an error with the message "Error test successful." Defaults to 
             false.
-
---log   If set to false, no log files are written. Defaults to true.
+--log		If set to false, no log files are written. Defaults to true.
             Logs are always written to database.
-
---help 	Return this message and exit.
+--help, -h	Return this message and exit.
 `)
 
 if (args.help || args.h) {
-  	console.log(help)
-  	process.exit(0)
+    console.log(help)
+    process.exit(0)
 }
 
-args['log']
-args['debug']
-args['port']
+var express = require('express')
+var app = express()
+const fs = require('fs')
+const morgan = require('morgan')
+const logdb = require('./src/services/database.js')
 
-const debug = args.debug || 'false';
-
+app.use(express.json());
+const port = args.port || args.p || process.env.PORT || 5000
 if (args.log == 'false') {
     console.log("NOTICE: not creating file access.log")
-} else {
+} 
+else {
     const logdir = './log/';
 
     if (!fs.existsSync(logdir)){
@@ -53,47 +40,36 @@ if (args.log == 'false') {
 }
 
 app.use((req, res, next) => {
-  	// Your middleware goes here.
-  	let logdata = {
-    	remoteaddr: req.ip,
-    	remoteuser: req.user,
-    	time: Date.now(),
-    	method: req.method,
-    	url: req.url,
-    	protocol: req.protocol,
-    	httpversion: req.httpVersion,
-    	status: res.statusCode,
-    	referer: req.headers['referer'],
-    	useragent: req.headers['user-agent']
-  	}
-  	const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url,  protocol, httpversion, secure, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
-  	stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.secure, logdata.status, logdata.referer, logdata.useragent);
-  	next();
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referrer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    };
+    const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
+    console.log(info)
+    next();
 })
-
-if (debug == true) {
-  	app.get('/app/log/access', (req, res) => {
-    	// userinfo or accesslog?
-    	const stmt = db.prepare('SELECT * FROM userinfo').all()
-    	res.status(200).json(stmt)
-  	});
-  	app.get('/app/error', (req, res) => {
-    	res.status(500)
-    	throw new Error('Error test successful') // Express will catch this on its own.
-  	});
-}
 
 // Previous API Construction from last assignment
 
-app.get('/app/', (req, res) => {
-  	// Respond with status 200
-  	res.statusCode = 200;
-  	// Respond with status message "OK"
-  	res.statusMessage = 'OK';
-  	//res.send('Hello World')
-  	// res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-  	// res.end(res.statusCode+ ' ' +res.statusMessage);
-})
+app.use(express.static('./public'))
+
+// READ (HTTP method GET) at root endpoint /app/
+app.get("/app/", (req, res, next) => {
+    res.json({"message":"Your API works! (200)"});
+	res.status(200);
+});
+
+// Endpoint /app/flip/ that returns JSON {"flip":"heads"} or {"flip":"tails"} 
+// corresponding to the results of the random coin flip.
 app.get('/app/flip/', (req, res) => {
     const flip = coinFlip()
     res.status(200).json({ "flip" : flip })
@@ -131,6 +107,13 @@ if (args.debug || args.d) {
         throw new Error('Error test works.')
     })
 }
+
+// Default API endpoint that returns 404 Not found for any endpoints that are not defined.
+app.use(function(req, res){
+    const statusCode = 404
+    const statusMessage = 'NOT FOUND'
+    res.status(statusCode).end(statusCode+ ' ' +statusMessage)
+});
 
 function coinFlip() {
 	var num = Math.floor(Math.random()*100);
@@ -179,10 +162,9 @@ app.use(function(req, res){
     const statusMessage = 'NOT FOUND'
     res.status(statusCode).end(statusCode+ ' ' +statusMessage)
 });
-
 // Start server
-const server = app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+const server = app.listen(port, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",port))
 });
 // Tell STDOUT that the server is stopped
 process.on('SIGINT', () => {
